8b599229656725f2d665af45ef14b45ded8a439a
Joshua Bidwell <jjbidwell262@gmail.com>
2020-10-11 10:56:33 -0700

    Connected database to server.js

--- server.js
+++ server.js
@@ -2,3 +2,31 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 require('console.table');
 
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'password',
+    database : 'employee_tracker'
+  });
+
+  connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+   
+    console.log('connected as id ' + connection.threadId);
+    afterConnection();
+
+  });
+
+  function afterConnection() {
+    // perform a select query and print the result in the console
+    connection.query("SELECT * FROM employee", (err, res) => {
+      if (err) {
+        throw err;
+      }
+      console.table(res);
+
+    });
+  }
