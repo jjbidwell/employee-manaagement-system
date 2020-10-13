6997af6de4599d0401db011a74a35f583763592e
Joshua Bidwell <jjbidwell262@gmail.com>
2020-10-12 23:29:18 -0700

    The program can now update employee roles

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 require('console.table');
 
+let roleID;
+let managerID = null;
 let employeeArray = [];
+let roleArray = ['Executive', 'Sales Manager', 'Sales Lead', 'Salesperson', 'Engineering Manager', 'Engineering Lead', 'Engineer', 'Head Lawyer', 'Lawyer', 'Head Accountant', 'Accountant', 'Human Resource Manager', 'Human Resource Agent'];
 
 const connection = mysql.createConnection({
     host     : 'localhost',
@@ -16,23 +19,11 @@ const connection = mysql.createConnection({
       console.error('error connecting: ' + err.stack);
       return;
     }
-
     console.log('connected as id ' + connection.threadId);
     populateArray();
     begin();
   });
 
-function populateArray(){
-  employeeArray = [];
-  connection.query('SELECT first_name, last_name FROM employees ORDER BY last_name;', (err, res) => {
-    if (err) {
-      throw err;
-    }
-      res.forEach(element => {
-        employeeArray.push(element.last_name + ", " + element.first_name);
-      });
-    });
-  };
 
   const allQuery = `SELECT e.id, e.last_name AS 'Last Name', e.first_name AS 'First Name', r.title AS 'Job', d.name AS 'Department', e.manager_id AS 'Manager' 
   FROM employees AS e
@@ -59,6 +50,10 @@ function populateArray(){
 
   const addQuery = `INSERT INTO employees(first_name, last_name, role_id, manager_id)
   VALUES(?, ?, ?, ?);`
+
+  const updateQuery = `UPDATE employees
+  SET role_id = ?, manager_id = ?
+  WHERE first_name = ? AND last_name = ?;`
 
   const deleteQuery = `DELETE FROM employees WHERE first_name = ? AND last_name = ?;`
 //DONE
@@ -103,6 +98,68 @@ function populateArray(){
       });
   };
 
+  function populateArray(){
+    employeeArray = [];
+    connection.query('SELECT first_name, last_name FROM employees ORDER BY last_name;', (err, res) => {
+      if (err) {
+        throw err;
+      }
+        res.forEach(element => {
+          employeeArray.push(element.last_name + ", " + element.first_name);
+        });
+      });
+    };
+  
+function chooseRole(x){
+  switch(x) {
+    case 'Executive':
+      roleID = 1;
+      break;
+    case 'Sales Manager':
+      roleID = 2;
+      break;
+    case "Sales Lead":
+      roleID = 3;
+      managerID = 2;
+      break;
+    case 'Salesperson':
+      roleID = 4;
+      managerID = 2;
+      break;
+    case 'Engineering Manager':
+      roleID = 5;
+      break;
+    case "Engineering Lead":
+      roleID = 6;
+      managerID = 3;
+      break;
+    case 'Engineer':
+      roleID = 7;
+      managerID = 3;
+      break;
+    case 'Head Lawyer':
+      roleID = 8;
+      break;
+   case "Lawyer":
+      roleID = 9;
+      managerID = 4;
+      break;
+  case 'Head Accountant':
+      roleID = 10;
+      break;
+  case 'Accountant':
+      roleID = 11;
+      managerID = 5
+      break;
+  case "Human Resources Manager":
+      roleID = 12;
+      break;
+  case "Human Resources Agent":
+      roleID = 13;
+      managerID = 6
+      break;
+  }
+}
 //DONE
   function viewEmployees(){
     connection.query(allQuery, (err, res) => {
@@ -153,21 +210,7 @@ function populateArray(){
           type: 'list',
           message: "What will be the new employee's job?",
           name: 'role',
-          choices: [
-            'Executive',
-            'Sales Manager',
-            'Sales Lead',
-            'Salesperson',
-            'Engineering Manager',
-            'Engineering Lead',
-            'Engineer',
-            'Head Lawyer',
-            'Lawyer',
-            'Head Accountant',
-            'Accountant',
-            'Human Resource Manager',
-            'Human Resource Agent'
-          ]
+          choices: roleArray
         },
         {
           type: "input",
@@ -181,70 +224,25 @@ function populateArray(){
         }
       ]).then(response => {
         //let role = response.role;
-        let roleID;
+
         let firstName = response.first;
         let lastName = response.last;
-        let managerID = null;
 
-        switch(response.role) {
-          case 'Executive':
-            roleID = 1;
-            break;
-          case 'Sales Manager':
-            roleID = 2;
-            break;
-          case "Sales Lead":
-            roleID = 3;
-            managerID = 2;
-            break;
-          case 'Salesperson':
-            roleID = 4;
-            managerID = 2;
-            break;
-          case 'Engineering Manager':
-            roleID = 5;
-            break;
-          case "Engineering Lead":
-            roleID = 6;
-            managerID = 3;
-            break;
-          case 'Engineer':
-            roleID = 7;
-            managerID = 3;
-            break;
-          case 'Head Lawyer':
-            roleID = 8;
-            break;
-         case "Lawyer":
-            roleID = 9;
-            managerID = 4;
-            break;
-        case 'Head Accountant':
-            roleID = 10;
-            break;
-        case 'Accountant':
-            roleID = 11;
-            managerID = 5
-            break;
-        case "Human Resources Manager":
-            roleID = 12;
-            break;
-        case "Human Resources Agent":
-            roleID = 13;
-            managerID = 6
-            break;
-        }
+        chooseRole(response.role)
+        
         connection.query(addQuery, [firstName, lastName, roleID, managerID], (err, res) => {
           if (err) {
             throw err;
           }
-          console.log('Employee Added!');
+          console.log(`${firstName} ${lastName} Added!`);
           populateArray();
           begin();
         });
 
       })
   }
+
+  //DONE
   function removeEmployee(){
       inquirer
         .prompt([
@@ -261,9 +259,7 @@ function populateArray(){
           }
         ]).then(response => {
           if(response.confirm === true){
-            let nameArray = response.removeName.split(', ');;
-            let first;
-            let last;
+            let nameArray = response.removeName.split(', ');
             connection.query(deleteQuery, [nameArray[1], nameArray[0]], (err, res) => {
               if (err) {
                 throw err;
@@ -279,8 +275,36 @@ function populateArray(){
   }
 
   function updateRole(){
-
+    inquirer 
+      .prompt([
+        {
+          type: 'list',
+          message: 'What employee would you like to update?',
+          name: 'changeRole',
+          choices: employeeArray
+        },
+        {
+          type: 'list',
+          message: 'What role would you like to switch them to?',
+          name: 'role',
+          choices: roleArray
+        }
+      ]).then(response => {
+        //console.log(response);
+        let nameArray = response.changeRole.split(', ');
+        chooseRole(response.role);
+        connection.query(updateQuery, [roleID, managerID, nameArray[1], nameArray[0]], (err, res) => {
+          if (err) {
+            throw err;
+          }
+          console.log(`${nameArray[1]} ${nameArray[0]} Updated!`);
+          populateArray();
+          begin();
+        });
+      })
   }
+
+
   function updateManager(){
 
   }
