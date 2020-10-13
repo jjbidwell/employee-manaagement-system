a9032d1abdde677f0c64c5ea7dc812b4dc1c6d89
Joshua Bidwell <jjbidwell262@gmail.com>
2020-10-12 17:31:27 -0700

    Added initial inquirer prompt and switch

--- server.js
+++ server.js
@@ -16,17 +16,86 @@ const connection = mysql.createConnection({
     }
    
     console.log('connected as id ' + connection.threadId);
-    afterConnection();
+    begin();
+    //afterConnection();
 
   });
 
   function afterConnection() {
     // perform a select query and print the result in the console
-    connection.query("SELECT * FROM employee", (err, res) => {
+    connection.query("SELECT * FROM employees", (err, res) => {
       if (err) {
         throw err;
       }
       console.table(res);
 
     });
+  }
+
+  function begin(){
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "What would you like to do?",
+          name: 'choice',
+          choices: [
+            'View all employees',
+            'View all employees by department',
+            'View all employees by manager',
+            'Add employee', 
+            'Remove employee',
+            'Update employee role',
+            'Update employee manager',
+            'View all roles'
+          ]
+        }
+      ]).then(answers => {
+        console.log(answers.choice);
+        switch (answers.choice){
+          case 'View all employees':
+            return viewEmployees();
+          case 'View all employees by department':
+            return viewByDepartment();
+          case 'View all employees by manager':
+            return viewByManager();
+          case 'Add employee':
+            return addEmployee();
+          case 'Remove employee':
+            return removeEmployee();
+          case 'Update employee role':
+            return updateRole();
+          case 'Update employee manager':
+            return updateManager();
+          case 'View all roles':
+            return viewRoles();
+            
+        };
+      });
+  };
+
+
+  function viewEmployees(){
+
+  }
+  function viewByDepartment(){
+
+  }
+  function viewByManager(){
+
+  }
+  function addEmployee(){
+
+  }
+  function removeEmployee(){
+
+  }
+  function updateRole(){
+
+  }
+  function updateManager(){
+
+  }
+  function viewRoles(){
+
   }
